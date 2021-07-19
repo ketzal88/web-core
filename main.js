5b94b79955ab995e7ad2b7fa7d5c1ddb038693f2
matias_gomez <mfgomez.810@gmail.com>
2021-07-18 21:15:33 -0300

    add slider testimonios

--- main.js
+++ main.js
@@ -9,3 +9,23 @@ function myFunction() {
       y.innerText= 'Ver Más'
     }
   }
+const buttonsWrapper = document.querySelector(".map");
+const slides = document.querySelector(".inner");
+
+buttonsWrapper.addEventListener("click", e => {
+  if (e.target.nodeName === "BUTTON") {
+    Array.from(buttonsWrapper.children).forEach(item =>
+      item.classList.remove("active")
+    );
+    if (e.target.classList.contains("first")) {
+      slides.style.transform = "translateX(-0%)";
+      e.target.classList.add("active");
+    } else if (e.target.classList.contains("second")) {
+      slides.style.transform = "translateX(-20em)";
+      e.target.classList.add("active");
+    } else if (e.target.classList.contains('third')){
+      slides.style.transform = 'translatex(-40em)';
+      e.target.classList.add('active');
+    }
+  }
+});
