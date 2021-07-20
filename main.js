5a8f3fbc0b0af8deae9d79c5546500e0b95762a7
matias_gomez <mfgomez.810@gmail.com>
2021-07-20 08:48:49 -0300

    fix burgerMenu

--- main.js
+++ main.js
@@ -1,21 +1,20 @@
 function myFunction() {
-    var x = document.getElementById("list");
-    var y = document.getElementById("button");
-    if (x.style.display === "none") {
-      x.style.display = "flex";
-      y.innerText= 'Ver Menos'
-    } else {
-      x.style.display = "none";
-      y.innerText= 'Ver Más'
-    }
+  var x = document.getElementById("list");
+  var y = document.getElementById("button");
+  if (x.style.display === "none") {
+    x.style.display = "flex";
+    y.innerText = "Ver Menos";
+  } else {
+    x.style.display = "none";
+    y.innerText = "Ver Más";
   }
+}
 const buttonsWrapper = document.querySelector(".map");
 const slides = document.querySelector(".inner");
 
-buttonsWrapper.addEventListener("click", e => {
+buttonsWrapper.addEventListener("click", (e) => {
   if (e.target.nodeName === "BUTTON") {
-    console.log('click')
-    Array.from(buttonsWrapper.children).forEach(item =>
+    Array.from(buttonsWrapper.children).forEach((item) =>
       item.classList.remove("active")
     );
     if (e.target.classList.contains("first")) {
@@ -24,17 +23,33 @@ buttonsWrapper.addEventListener("click", e => {
     } else if (e.target.classList.contains("second")) {
       slides.style.transform = "translateX(-20%)";
       e.target.classList.add("active");
-    } else if (e.target.classList.contains('third')){
-      slides.style.transform = 'translatex(-40%)';
-      e.target.classList.add('active');
-    }
-    else if (e.target.classList.contains('fourth')){
-      slides.style.transform = 'translatex(-60%)';
-      e.target.classList.add('active');
-    }
-    else if (e.target.classList.contains('fifth')){
-      slides.style.transform = 'translatex(-80%)';
-      e.target.classList.add('active');
+    } else if (e.target.classList.contains("third")) {
+      slides.style.transform = "translatex(-40%)";
+      e.target.classList.add("active");
+    } else if (e.target.classList.contains("fourth")) {
+      slides.style.transform = "translatex(-60%)";
+      e.target.classList.add("active");
+    } else if (e.target.classList.contains("fifth")) {
+      slides.style.transform = "translatex(-80%)";
+      e.target.classList.add("active");
     }
   }
 });
+const burgerMenu = document.querySelector("#burgerMenu");
+const sidebar = document.querySelector("#sidebar");
+const btnClose = document.querySelector("#btn-close");
+const listClose = document.querySelector("#list-close");
+
+function open() {
+  if (sidebar.classList.contains("show")) {
+    sidebar.classList.remove("show");
+  } else {
+    sidebar.classList.add("show");
+  }
+}
+burgerMenu.addEventListener("click", open);
+btnClose.addEventListener("click", open);
+Array.from(listClose.children).forEach((item) => item.addEventListener("click", open));
+
+
+
